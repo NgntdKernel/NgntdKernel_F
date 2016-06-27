f2a7637d29b937aec1d8be6d5e08f353dd610f5b
flar2 <asegaert@gmail.com>
2016-06-26 23:06:11 -0400

    mdss_fb: add backlight dimmer option
    Signed-off-by: BijiOnta <sengolcaduk32@gmail.com>
    Signed-off-by: Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -86,6 +86,11 @@
  * Default value is set to 1 sec.
  */
 #define MDP_TIME_PERIOD_CALC_FPS_US	1000000
+#define MDSS_BRIGHT_TO_BL_DIM(out, v) do {\
+			out = (12*v*v+1393*v+3060)/4465;\
+			} while (0)
+bool backlight_dimmer = false;
+module_param(backlight_dimmer, bool, 0755);
 
 static struct fb_info *fbi_list[MAX_FBI_LIST];
 static int fbi_list_index;
@@ -340,6 +345,15 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 		brightness_min, mfd->panel_info->brightness_max);
 		if (bl_lvl && !value)
 			bl_lvl = 0;
+
+	if (backlight_dimmer) {
+		MDSS_BRIGHT_TO_BL_DIM(bl_lvl, value);
+	} else {
+		/* This maps android backlight level 0 to 255 into
+		   driver backlight level 0 to bl_max with rounding */
+		MDSS_BRIGHT_TO_BL(bl_lvl, value, mfd->panel_info->bl_max,
+					mfd->panel_info->brightness_max);
+	}
 
 	if (!bl_lvl && value)
 		bl_lvl = 1;
